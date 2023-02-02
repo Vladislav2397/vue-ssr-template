86236cc269fdc644421e99d44f4d3bf6cb4a8070
Vladislav2397 <53734286+Vladislav2397@users.noreply.github.com>
2023-02-02 10:34:51 +0300

    Add route About

--- src/app/providers/router/index.js
+++ src/app/providers/router/index.js
@@ -1,19 +1,12 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-
-import Home from '@/pages/Home/Home.vue'
+import { routes } from './routes'
 
 Vue.use(Router)
 
 export function createRouter() {
     return new Router({
         mode: 'history',
-        routes: [
-            {
-                path: '/',
-                name: 'Index',
-                component: Home,
-            },
-        ],
+        routes,
     })
 }
---
+++ src/pages/About/About.vue
@@ -0,0 +1,10 @@
+<template lang="pug">
+.b-about
+    h1.__text About page
+</template>
+
+<script>
+export default {
+    name: 'About',
+}
+</script>
